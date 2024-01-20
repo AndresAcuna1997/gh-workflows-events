c45d4f32ec5ac299e45d96703c0ff721bc306f51
Andres Acuna <andres.acu1997@gmail.com>
2024-01-20 16:04:48 -0500

    New JSX

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,7 @@ function App() {
           <img src={logo} />
         </div>
         <h1>Learn & Master GitHub Actions</h1>
+        <h1>Section 03</h1>
       </header>
       <MainContent />
     </>
